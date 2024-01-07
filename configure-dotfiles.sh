68cf48630072922cf5aa735083ff6a9086fed084
brunordg <3471592+brunordg@users.noreply.github.com>
2024-01-06 23:10:20 -0300

    Alteração no arquivo shell script

--- configure-dotfiles.sh
+++ configure-dotfiles.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.sdkmanrc ~/.sdkmanrc
+#ln -s ~/.dotfiles/.zshrc ~/.zshrc
+#ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+#ln -s ~/.dotfiles/.sdkmanrc ~/.sdkmanrc
+
+DIR="$HOME/.zshrc"
+if [ ! -L "$DIR" ]; then  
+    ln -s ~/.dotfiles/.zshrc ~/.zshrc
+    echo "Create simbolic link ${DIR} ..."
+else  
+    echo "Error: ${DIR} exists"    
+fi
+
+DIR="$HOME/.gitconfig"
+if [ ! -L "$DIR" ]; then  
+    ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+    echo "Create simbolic link ${DIR} ..."
+else  
+    echo "Error: ${DIR} exists"    
+fi
+
+DIR="$HOME/.sdkmanrc"
+if [ ! -L "$DIR" ]; then  
+    ln -s ~/.dotfiles/.sdkmanrc ~/.sdkmanrc
+    echo "Create simbolic link ${DIR} ..."
+else  
+    echo "Error: ${DIR} exists"    
+fi
