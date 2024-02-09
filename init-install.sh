6a67f1d589215950a67eb0292bd2cc3485354603
Bruno Rodrigues <rodrigues.brodrigues@gmail.com>
2024-02-09 00:02:21 -0300

    Criar um arquivo para fazer a instalacao inicial

---
+++ init-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt install curl -y
+sudo apt install git
+sudo apt install zsh
+sudo snap install code --classic
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install datagrip --classic
+sudo snap install docker
+sudo snap install sublime-text --classic
+sudo snap install kubectl --classic
+curl -s "https://get.sdkman.io" | bash
+
