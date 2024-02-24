93d6a45ddce8772c3f502d650e60f8f90906d568
brunordg <3471592+brunordg@users.noreply.github.com>
2024-02-24 20:33:04 -0300

    Alteracao nas aplicacoes defaults

--- init-install.sh
+++ init-install.sh
@@ -11,5 +11,8 @@ sudo snap install datagrip --classic
 sudo snap install docker
 sudo snap install sublime-text --classic
 sudo snap install kubectl --classic
+sudo snap install node --classic
+sudo snap install httpie
+sudo snap install miro
 curl -s "https://get.sdkman.io" | bash
 
